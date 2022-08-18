ff184b378be2ff92c11035f7bc511d78e76563f9
DylanParenzee <dylanparenzee@hotmail.com>
2022-08-19 07:06:29 +1000

    added fetch functionailty, loading functionality and error handling and display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 
-const App = () => {
+const url = "https://course-api.com/react-tours-project";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchData = () => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setLoading(false);
+        setTours(data);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <main>
+      <Tours tours={tours} />
+    </main>
   );
-};
+}
 
 export default App;
